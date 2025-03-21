f8fa1c9a3b2fc2f9a0ed19c8ec941c00b913e367
Andy <andygonzales.2005@outlook.es>
2025-03-21 16:14:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/proyectofinal/Conexion/Conexion.kt
@@ -0,0 +1,6 @@
+package com.example.proyectofinal.Conexion
+
+object ConexionTmdb {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "955eded0424a5fd5226893935b6da43a"
+}
